d021ada376a6f00b416b21a3ec658adc99dc4402
Morii Shikaa <ramanavami32@gmail.com>
2021-02-02 12:35:26 +0800

    able to play a song only with url

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const moriiuta = new Discord.Client();
 const TOKEN = process.env.TOKEN;
-
-const pesan = new Discord.Message();
+const ytdl = require('ytdl-core-discord');
+const fs = require('fs')
 
 moriiuta.login(TOKEN);
 
@@ -11,21 +11,26 @@ moriiuta.on('ready', () => {
     console.info(`Logged in as ${moriiuta.user.tag}!`);
 });
 
-
+async function play(connection, url) {
+  connection.play(await ytdl(url), { type: 'opus' });
+}
 moriiuta.on('message', async msg => {
     if(msg.content === "hello uta chan"){
         return msg.channel.send(`yahoooo 👋🎶 ${msg.author.username}-sama`)
     }
 
-    if(msg.content.startsWith("uta chan play")){
+    if(msg.content.startsWith("uta chan sing")){
+        const connection = await msg.member.voice.channel.join();
+        const url = msg.content.split(" ");
         if(msg.member.voice.channel){
-            const connection = await msg.member.voice.channel.join();
-            const url = msg.content.split(" ");
-            const dispatch = connection.play(url[3])
+            play(connection, url[3])
         }
+    }else if(msg.content.startsWith("uta chan arigatou")){
+        const connection = await msg.member.voice.channel;
+         msg.channel.send(`jaa naaa arigatou mina`)
+        return connection.leave();
     }
 })
-
 
 // let channel = msg.member.voice.channel;
 //         if(channel && channel.type === 'voice') {
